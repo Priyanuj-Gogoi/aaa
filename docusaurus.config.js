43c0aeda2d8248081c069d2ae1d8f0f0f2860e92
Priyanuj09 <priyanuj289@gmail.com>
2022-12-08 18:50:03 +0000

    fix: failed deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,67 +2,66 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://priyanuj-gogoi.github.io', // Change
-  baseUrl: '/aaa/', // Change
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-	onDuplicateRoutes: 'throw',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Priyanuj Gogoi', // Change
-  projectName: 'aaa', // Change
+	title: 'My Site',
+	tagline: 'Dinosaurs are cool',
+	url: 'https://priyanuj-gogoi.github.io', // Change
+	baseUrl: '/aaa/', // Change
+	onBrokenLinks: 'warn',
+	onBrokenMarkdownLinks: 'warn',
+	onDuplicateRoutes: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'Priyanuj Gogoi', // Change
+	projectName: 'aaa', // Change
 	trailingSlash: false,
 	noIndex: true,
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-  presets: [
-    [
-      'classic',
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/priyanuj-gogoi/aaa/edit/master/', // Change
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
+	presets: [
+		[
+			'classic',
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/priyanuj-gogoi/aaa/edit/master/', // Change
 					routeBasePath: '/',
 					showLastUpdateTime: true,
-          showLastUpdateAuthor: true
-        },
-        blog: false,
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-  themeConfig:
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+					showLastUpdateAuthor: true
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
+	themeConfig:
+		({
+			navbar: {
+				title: 'My Site',
+				logo: {
+					alt: 'My Site Logo',
+					src: 'img/logo.svg',
+				},
 				hideOnScroll: true,
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+				items: [
+					{
+						type: 'doc',
+						docId: 'intro',
+						position: 'left',
+						label: 'Tutorial',
+					},
+					{
+						href: 'https://github.com/facebook/docusaurus',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
