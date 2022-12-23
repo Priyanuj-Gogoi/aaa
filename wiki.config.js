2ccc98c4913adf8fdf2b9e492bae5258c473aa59
প্রিয়ানুজ গগৈ <priyanuj289@gmail.com>
2022-12-23 11:18:48 +0530

    chore: environment variables

--- wiki.config.js
+++ wiki.config.js
@@ -1,11 +1,12 @@
 const {
-  ORG, REPO, BRANCH
+  ORGANIZATION_NAME, PROJECT_NAME, DEPLOYMENT_BRANCH, PROJECT_BRANCH
 } = process.env;
 
 module.exports = {
-  title: 'BDFD Wiki',
+  title: 'BDFD Docs',
   description: 'Soon...Rewriting',
-  user: ORG || 'priyanuj-gogoi',
-  repo: REPO || 'aaa',
-  branch: BRANCH || 'main'
+  user: ORGANIZATION_NAME || 'priyanuj-gogoi',
+  repo: PROJECT_NAME || 'aaa',
+  deployBranch: DEPLOYMENT_BRANCH || 'gh-pages',
+  branch: PROJECT_BRANCH || 'main'
 };
